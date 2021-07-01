5c33b287ebc6a9e4bbd50b1b7df07239bb2c755d
Muhammed shahas <Mshahas7886@gmail.com>
2021-07-02 05:03:08 +1200

    masonry working

--- js/script.js
+++ js/script.js
@@ -640,7 +640,7 @@ $(document).ready(function () {
                          ///////projects////////////////////////////////
                          '<div class="container">'+
 
-                         '<div data-masonry='/{"percentPosition": true }/' class="row row-cols-1 row-cols-md-3 g-4">'+
+                         '<div data-masonry=\'{"percentPosition": true }\' class="row row-cols-1 row-cols-md-3 g-4">'+
 
 
                             '<div class="col">'+
